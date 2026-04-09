647022691b6d14f41f27cbaeede4784837a245b9
Merizio <meriziofrancisco@gmail.com>
2026-04-09 10:48:10 -0300

    Configuracao maven concluida

--- learnmvn/src/main/java/br/ufes/mvn/App.java
+++ learnmvn/src/main/java/br/ufes/mvn/App.java
@@ -1,13 +1,33 @@
 package br.ufes.mvn;
 
+import java.io.FileReader;
+import java.io.Reader;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws Exception
     {
         System.out.println( "Hello World!" );
+
+        //Tree<> 
+
+        Reader in = new FileReader("female-and-male-life-expectancy-at-birth-in-years.csv");
+        Iterable<CSVRecord> records = CSVFormat.RFC4180.parse(in);
+        for (CSVRecord record : records) {
+            String Year = record.get(2);
+            if(Year.equals("2020")){
+                String country = record.get(0);
+                String age = record.get(3);
+
+                System.err.println("País:"+country+" Expectativa:"+age);
+            }
+        }
     }
 }
